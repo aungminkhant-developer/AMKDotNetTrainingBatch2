8365a4a66443ef4fd408a807da59b64d526e1de9
Aung Min Khant <aungminkhant.aln01@gmail.com>
2025-06-14 20:15:39 +0630

    Feat ADO.Net CRUD

--- AMKDotNetTrainingBatch2.ConsoleApp/Program.cs
+++ AMKDotNetTrainingBatch2.ConsoleApp/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AMKDotNetTrainingBatch2.ConsoleApp;
+using Microsoft.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create();
+adoDotNetExample.Edit();
+adoDotNetExample.Update();
+
